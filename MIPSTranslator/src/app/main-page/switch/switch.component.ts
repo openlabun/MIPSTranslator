a4a4cf497b0501b524d3adac3132da177090237d
CristhianAc <cristhian_agamez@outook.com>
2024-08-14 16:00:45 -0500

    chore: Update textbox component to use reactive form control

--- MIPSTranslator/src/app/main-page/switch/switch.component.ts
+++ MIPSTranslator/src/app/main-page/switch/switch.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output} from '@angular/core';
+import { Component, EventEmitter, output, Output} from '@angular/core';
 
 @Component({
   selector: 'app-switch',
@@ -8,12 +8,13 @@ import { Component, EventEmitter, Output} from '@angular/core';
   styleUrl: './switch.component.css'
 })
 export class SwitchComponent {
-  @Output() toggleChange = new EventEmitter<boolean>();
+  toggleChange = output<boolean>();
 
   isHexToMIPS: boolean = true; 
 
   onToggle(): void {
     this.isHexToMIPS = !this.isHexToMIPS;
+    
     this.toggleChange.emit(this.isHexToMIPS);
   }
 }
