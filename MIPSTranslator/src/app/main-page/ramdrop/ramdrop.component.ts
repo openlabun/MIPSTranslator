9663e820aa88d0c2a71b07580e695732d326f2b5
CristhianAc <cristhian_agamez@outlook.com>
2024-08-19 23:56:02 -0500

    chore: Remove unused font imports and update project configuration

--- MIPSTranslator/src/app/main-page/ramdrop/ramdrop.component.ts
+++ MIPSTranslator/src/app/main-page/ramdrop/ramdrop.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-ramdrop',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './ramdrop.component.html',
   styleUrl: './ramdrop.component.css'
 })
 export class RamdropComponent {
+  file : File | null = null;
+  fileForm = new FormControl(null, []);
 
+  constructor() {
+    this.fileForm.valueChanges.subscribe((value: File | null) => {
+      if (value){
+        this.file = value;
+      }
+      
+    });
+  }
+
+
+  getFile(event: Event): void {
+    event.preventDefault();
+    const inputEvent = (event.target as HTMLInputElement);
+    if (inputEvent.files && inputEvent.files.length > 0) {
+      this.file = inputEvent.files[0];
+      console.log(this.file);
+    }
+
+  }
 }
