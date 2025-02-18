fb67cdc2a4ce9f3e2f5acc76e9b2fb46e123eb87
Augusto Salazar SIlva <augustosalazar@uninorte.edu.co>
2025-02-18 15:27:55 -0500

    include github contributors

--- app/src/app.component.ts
+++ app/src/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MainPageComponent } from "./main-page/main-page.component";
+import { ContributorsComponent } from './contributors/contributors.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MainPageComponent],
+  imports: [RouterOutlet, MainPageComponent, ContributorsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
