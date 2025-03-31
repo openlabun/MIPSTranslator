ff97d03532d63ad0da8e9e52250b8c8025b8ab39
YourOrdinaryCat <49221554+YourOrdinaryCat@users.noreply.github.com>
2025-03-31 15:50:54 -0500

    Add load RAM button

--- app/src/main-page/main-page.component.ts
+++ app/src/main-page/main-page.component.ts
@@ -1,4 +1,11 @@
-import { Component, inject, model } from '@angular/core';
+import {
+  afterNextRender,
+  ChangeDetectorRef,
+  Component,
+  inject,
+  model,
+} from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import {
   AutoSuggestBoxComponent,
   QuerySubmittedEvent,
@@ -12,22 +19,71 @@ import { DecodedInstruction } from '../Shared/lib/mips/instruction';
 import { AssistantService } from '../Shared/Services/Assistant/assistant.service';
 import { TranslatorService } from '../Shared/Services/Translator/translator.service';
 
+function onDragEnter(e: DragEvent) {
+  e.preventDefault();
+  const dt = e.dataTransfer;
+
+  if (!dt) {
+    return;
+  }
+
+  if (dt.items.length === 1 && dt.items[0].kind === 'file') {
+    dt.dropEffect = 'copy';
+    document.body.toggleAttribute('drag', true);
+  } else {
+    dt.dropEffect = 'none';
+  }
+}
+
+function onDragLeave() {
+  document.body.toggleAttribute('drag', false);
+}
+
+type DecodeSuccess = {
+  success: true;
+  result: {
+    decoded: DecodedInstruction[];
+    unsupported: string[];
+  };
+};
+
+type DecodeFailure = {
+  success: false;
+  error: string;
+};
+
+type DecodeResult = DecodeSuccess | DecodeFailure;
+
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [AutoSuggestBoxComponent, ListViewComponent, MipsDetailComponent],
+  imports: [
+    AutoSuggestBoxComponent,
+    ListViewComponent,
+    MipsDetailComponent,
+    ReactiveFormsModule,
+  ],
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.css'],
 })
 export class MainPageComponent {
   private readonly assistant = inject(AssistantService);
   private readonly translator = inject(TranslatorService);
+  private readonly dropHandler = this.onDrop.bind(this);
 
   readonly instructions: DecodedInstruction[] = [];
   readonly suggestions: string[] = [];
 
   textInput = model<string>('');
   selected: DecodedInstruction | undefined = undefined;
+
+  constructor(private readonly changes: ChangeDetectorRef) {
+    afterNextRender(() => {
+      document.addEventListener('dragover', onDragEnter);
+      document.addEventListener('dragleave', onDragLeave);
+      document.addEventListener('drop', this.dropHandler);
+    });
+  }
 
   onInputUpdated(newValue: string) {
     this.suggestions.splice(0, this.suggestions.length);
@@ -43,10 +99,10 @@ export class MainPageComponent {
       return;
     }
 
-    this.textInput.set('');
     const parsed = this.translator.tryParse(e.queryText);
 
     if (parsed.stage === 'complete') {
+      this.textInput.set('');
       this.instructions.push(parsed.instruction);
       return;
     }
@@ -87,5 +143,88 @@ export class MainPageComponent {
     anchor.click();
 
     window.URL.revokeObjectURL(anchor.href);
+  }
+
+  private decodeRam(file: File) {
+    return new Promise<DecodeResult>((resolve) => {
+      const reader = new FileReader();
+
+      reader.onload = (event) => {
+        const fileContent = event.target?.result as string;
+        const lines = fileContent.trim().split('\n');
+
+        if (lines.length < 2) {
+          resolve({
+            success: false,
+            error: 'Invalid file format. Expected at least two lines.',
+          });
+          return;
+        }
+
+        const instructions = lines[1].trim().split(/\s+/);
+        const decoded = [];
+        const unsupported = [];
+
+        for (const inst of instructions) {
+          const parsed = this.translator.tryParse(inst);
+
+          if (parsed.stage === 'complete') {
+            decoded.push(parsed.instruction);
+          } else {
+            unsupported.push(inst);
+          }
+        }
+        resolve({ success: true, result: { decoded, unsupported } });
+      };
+
+      reader.onerror = () => {
+        resolve({
+          success: false,
+          error: 'Unable to read file.',
+        });
+      };
+
+      reader.readAsText(file);
+    });
+  }
+
+  private async handleRamUpload(file: File) {
+    const decode = await this.decodeRam(file);
+
+    if (decode.success) {
+      this.instructions.splice(0, this.instructions.length);
+      this.instructions.push(...decode.result.decoded);
+
+      if (decode.result.unsupported.length > 0) {
+        alert(
+          `The following instructions couldn't be decoded: "${decode.result.unsupported.join(
+            '", "'
+          )}"`
+        );
+      }
+    } else {
+      alert(`Unable to load RAM: ${decode.error}`);
+    }
+  }
+
+  private async onDrop(e: DragEvent) {
+    e.preventDefault();
+    document.body.toggleAttribute('drag', false);
+    const item = e.dataTransfer?.items[0];
+
+    if (item?.kind === 'file') {
+      await this.handleRamUpload(item.getAsFile()!);
+      this.changes.detectChanges();
+    }
+  }
+
+  async onRamUpload(e: Event) {
+    e.preventDefault();
+    const inputEvent = e.target as HTMLInputElement;
+    const file = inputEvent.files?.item(0);
+
+    if (file) {
+      await this.handleRamUpload(file);
+    }
   }
 }
